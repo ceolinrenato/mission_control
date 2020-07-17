449476a178cce95ac1ccdc9c906bdff9bf36cd68
Renato Ceolin <renato.ceolin@renatoceolin.com>
2020-07-17 18:56:10 -0300

    feat: add process error code when terminated for wrong reasons

--- lib/mission_control.ex
+++ lib/mission_control.ex
@@ -2,33 +2,52 @@ defmodule MissionControl do
   @moduledoc false
 
   def main(args \\ []) do
-    args
-    |> parse_args()
-    |> Enum.map(&MissionControl.InputParser.parse/1)
-    |> IO.inspect()
+    case parse_args(args) do
+      {:ok, input_paths} ->
+        input_paths
+        |> Enum.map(&MissionControl.InputParser.parse/1)
+        |> IO.inspect()
 
-    :ok
+        :ok
+
+      {:error, error} ->
+        IO.puts(error)
+
+        terminate_process()
+    end
   end
 
   defp parse_args(args) do
     with {_, input_paths, []} <- OptionParser.parse(args, strict: []),
          {:invalid_paths, []} <- {:invalid_paths, invalid_paths(input_paths)} do
-      input_paths
+      {:ok, input_paths}
     else
       {:invalid_paths, invalid_paths} ->
-        IO.puts("Could not read files at: #{Enum.join(invalid_paths, ", ")}.")
+        {:error, "Could not read files at: #{Enum.join(invalid_paths, ", ")}."}
 
       {_, _, invalid_args} ->
         invalid_arg_names =
           invalid_args
           |> Enum.map(fn {arg_name, _} -> arg_name end)
 
-        IO.puts("Invalid arguments: #{Enum.join(invalid_arg_names, ", ")}.")
+        {:error, "Invalid arguments: #{Enum.join(invalid_arg_names, ", ")}."}
     end
   end
 
   defp invalid_paths(input_paths) do
     valid_paths = Enum.filter(input_paths, &File.exists?/1)
     input_paths -- valid_paths
+  end
+
+  # For testing with ExUnit means we can't halt the process
+  # When running live is desirable that the executable terminates with a non 0 status for error probing
+  defp terminate_process() do
+    case function_exported?(Mix, :__info__, 1) do
+      true ->
+        :error
+
+      _ ->
+        System.halt(1)
+    end
   end
 end
--- test/mission_control_test.exs
+++ test/mission_control_test.exs
@@ -1,8 +1,19 @@
 defmodule MissionControlTest do
-  use ExUnit.Case
-  doctest MissionControl
+  use ExUnit.Case, async: false
 
-  test "greets the world" do
-    assert MissionControl.main() == :ok
+  import Mock
+
+  describe "main/1" do
+    test "returns error with invalid arguments" do
+      assert MissionControl.main(["--invalid_arg"]) == :error
+    end
+
+    test "returns okay with valid file path" do
+      file_path = "/any/path/file"
+
+      with_mock(File, exists?: fn ^file_path -> true end, read: fn ^file_path -> {:ok, ""} end) do
+        assert :ok = MissionControl.main([file_path])
+      end
+    end
   end
 end
