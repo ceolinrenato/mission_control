de8c23841e6e4e3470555722ac17ef6d6ca72fb9
Renato Ceolin <renato.ceolin@renatoceolin.com>
2020-07-17 18:28:16 -0300

    test: tests for the input parser model

---
+++ test/mission_control/input_parser_test.exs
@@ -0,0 +1,41 @@
+defmodule MissionControl.InputParserTest do
+  @moduledoc false
+  use ExUnit.Case, async: false
+
+  import Mock
+
+  describe "parse/1" do
+    setup do
+      file_content = ~s"""
+      5 5
+      1 2 N
+      LMLMLMLMM
+      3 3 E
+      MMRMMRMRRM
+
+      """
+
+      %{file_content: file_content}
+    end
+
+    test "should return the parsed content with valid file", %{file_content: file_content} do
+      file_path = "/any/path/file"
+
+      with_mock(File, read: fn ^file_path -> {:ok, file_content} end) do
+        assert %{
+                 land_size: {5, 5},
+                 probes: [
+                   %{
+                     initial_position: {1, 2, "N"},
+                     instructions: ["L", "M", "L", "M", "L", "M", "L", "M", "M"]
+                   },
+                   %{
+                     initial_position: {3, 3, "E"},
+                     instructions: ["M", "M", "R", "M", "M", "R", "M", "R", "R", "M"]
+                   }
+                 ]
+               } = MissionControl.InputParser.parse(file_path)
+      end
+    end
+  end
+end
